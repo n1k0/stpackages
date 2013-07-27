40b75806d6653034368d77263497c14b9f7c428b
Nicolas Perriault <nperriault@gmail.com>
2013-07-27 16:44:27 +0200

    db load on app start

--- README.md
+++ README.md
@@ -15,10 +15,18 @@ Import
 
 An import script is provided to import existing packages living in [Package Control](); you need to set the `GITHUB_API_TOKEN` env var to make it work:
 
-    $ GITHUB_API_TOKEN=gsdgg6hg54jh800bjb566 node import.js
+    $ GITHUB_API_TOKEN=gsdgg6hg54jh800bjb566 make import.js
 
 Run
 ---
+
+Development:
+
+You should use [nodemon](https://github.com/remy/nodemon), then:
+
+    $ nodemon app.js
+
+Production:
 
     $ node app.js
 
--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var path = require('path');
+var db = require('./lib/db');
 var app = express();
+
+var dataDir = path.join(__dirname, 'data');
 
 app.use(express.static(__dirname + "/static"));
 app.use(app.router);
@@ -9,4 +13,10 @@ app.start = function() {
   console.log("app started.");
 };
 
-app.start();
+db.load(dataDir, function(err, data) {
+  if (err)
+    return console.error("Couldn't load database: " + err);
+  console.log("data loaded, %d packages available.", data.length);
+  app.set("data", data);
+  app.start();
+});
