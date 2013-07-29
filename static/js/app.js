05308f4c6db0d31ef22ec3a12c06ed564d56bc54
Nicolas Perriault <nperriault@gmail.com>
2013-07-29 23:46:58 +0200

    basic navigation for recent, updated & popular package lists

--- static/js/app.js
+++ static/js/app.js
@@ -24,8 +24,9 @@ function md() {
   });
 }
 
-function PackageListCtrl($http, $scope) {
-  $http.get('/api/recent').success(function(packages) {
+function PackageListCtrl($http, $scope, $routeParams) {
+  var type = $routeParams.type || 'recent';
+  $http.get('/api/' + type).success(function(packages) {
     $scope.packages = packages;
   });
 }
@@ -33,7 +34,7 @@ function PackageListCtrl($http, $scope) {
 function PackageDetailsCtrl($http, $scope, $routeParams) {
   $http.get('/api/details/' + $routeParams.slug).success(function(pkg) {
     $scope['package'] = pkg; // yeah, package is a reserved word
-    setTimeout(md, 100); // wonder how to wait for angular to have set scope here
+    setTimeout(md); // wonder how to wait for angular to have set scope here
   });
 }
 
@@ -41,13 +42,17 @@ angular
   .module('SublimePackages', [])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: '/partials/list.html',
-        controller: PackageListCtrl
-      })
       .when('/details/:slug', {
         templateUrl: '/partials/details.html',
         controller: PackageDetailsCtrl
+      })
+      .when('/:type', {
+        templateUrl: '/partials/list.html',
+        controller: PackageListCtrl
+      })
+      .when('/search?q=:q', {
+        templateUrl: '/partials/list.html',
+        controller: PackageListCtrl
       })
       .otherwise({redirectTo: '/'});
   }]);
