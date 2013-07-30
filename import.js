c7adf58d4081058ddf4a446f3b6b717fc6f8b4b6
Nicolas Perriault <nperriault@gmail.com>
2013-07-30 16:59:55 +0200

    removed fixtures used during development

--- import.js
+++ import.js
@@ -1,7 +1,6 @@
 var importer = require('./lib/import');
 
-//var jsonDB = 'https://raw.github.com/wbond/package_control_channel/master/repositories.json';
-var jsonDB = 'http://localhost/stpackages/db-full.json';
+var jsonDB = 'https://raw.github.com/wbond/package_control_channel/master/repositories.json';
 
 importer.run(jsonDB, function(err) {
   if (err)
