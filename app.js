611fa8565607b2d4836beb09888d1665e11070b9
Nicolas Perriault <nperriault@gmail.com>
2013-08-03 19:41:38 +0200

    removed obsolete code related to db now we're using ES

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var http = require('http');
-var path = require('path');
-var db = require('./lib/db');
 var libsearch = require('./lib/search');
 var app = express();
-
-var dataDir = path.join(__dirname, 'data');
 
 app.use(express.static(__dirname + '/static'));
 app.use(app.router);
@@ -13,76 +9,36 @@ app.use(app.router);
 app.set('per page', ~~process.env.MAX_PER_PAGE || 12);
 app.set('json spaces', 2);
 
-var scoreScript = "_score * doc['nbStargazers'].value * doc['nbForks'].value " +
-                  " / (doc['nbIssues'].value + 1) * doc['popularity'].value";
-
-function createQuery(data) {
-  return new db.PackageQuery(data || app.get('data'));
-}
-
 app.get('/api/recent', function(req, res) {
-  /* jshint camelcase:false */
-  libsearch.sendSearchResults(res, {
-    from: ~~(req.query.offset || 0),
-    size: app.get('per page'),
-    sort: [{createdAt: "desc"}],
-    query: {match_all: {}}
+  libsearch.recentPackages(res, {
+    offset: ~~(req.query.offset || 0),
+    perPage: app.get('per page')
   });
 });
 
 app.get('/api/updated', function(req, res) {
-  /* jshint camelcase:false */
-  libsearch.sendSearchResults(res, {
-    from: ~~(req.query.offset || 0),
-    size: app.get('per page'),
-    sort: [{updatedAt: "desc"}],
-    query: {match_all: {}}
+  libsearch.updatedPackages(res, {
+    offset: ~~(req.query.offset || 0),
+    perPage: app.get('per page')
   });
 });
 
 app.get('/api/popular', function(req, res) {
-  /* jshint camelcase:false */
-  libsearch.sendSearchResults(res, {
-    from: ~~(req.query.offset || 0),
-    size: app.get('per page'),
-    //sort: [{popularity: "desc"}],
-    query: {
-      custom_score: {
-        query: {match_all: {}},
-        script: scoreScript
-      }
-    }
+  libsearch.popularPackages(res, {
+    offset: ~~(req.query.offset || 0),
+    perPage: app.get('per page')
   });
 });
 
 app.get('/api/search', function(req, res) {
-  /* jshint camelcase:false */
-  libsearch.sendSearchResults(res, {
-    from: ~~(req.query.offset || 0),
-    size: app.get('per page'),
-    query: {
-      custom_score: {
-        query: {
-          query_string : {
-            default_operator: "AND",
-            fields : [
-              "name^20",
-              "description^10",
-              "readme^.25",
-              "author"
-            ],
-            query : req.query.q,
-            use_dis_max : false
-          }
-        },
-        script: scoreScript
-      }
-    }
+  libsearch.searchPackages(res, req.query.q, {
+    offset: ~~(req.query.offset || 0),
+    perPage: app.get('per page')
   });
 });
 
 app.get('/api/details/:slug', function(req, res) {
-  res.send(createQuery().findFirstBySlug(req.params.slug));
+  libsearch.sendPackage(res, req.params.slug);
 });
 
 app.start = function() {
@@ -94,11 +50,6 @@ app.close = function(cb) {
   this._server.close(cb);
 };
 
-db.load(dataDir, function(err, data) {
-  if (err)
-    return console.error("Couldn't load database: " + err);
-  app.set('data', data);
-  app.start();
-});
+app.start();
 
 exports.app = app;
--- test/test.app.js
+++ test/test.app.js
@@ -1,39 +1,34 @@
 /* global describe, it, beforeEach, afterEach */
-var expect = require('chai').expect;
+//var expect = require('chai').expect;
 var sinon = require('sinon');
-var request = require('request');
-var db = require('../lib/db');
-var range = require('../lib/util').range;
+//var request = require('request');
 var app = require('../app').app;
 
 describe("App", function() {
   describe("routes", function() {
     var sandbox;
 
-    var http = {
-      get: function(path, cb) {
-        var url = 'http://0.0.0.0:3000' + path;
-        request.get(url, {}, function(err, res, body) {
-          if (err)
-            throw new Error('Unable to GET ' + url + ': ' + err.message);
-          cb({
-            headers: res.headers,
-            body: JSON.parse(body)
-          });
-        });
-      }
-    };
+    // var http = {
+    //   get: function(path, cb) {
+    //     var url = 'http://0.0.0.0:3000' + path;
+    //     request.get(url, {}, function(err, res, body) {
+    //       if (err)
+    //         throw new Error('Unable to GET ' + url + ': ' + err.message);
+    //       cb({
+    //         headers: res.headers,
+    //         body: JSON.parse(body)
+    //       });
+    //     });
+    //   }
+    // };
 
     beforeEach(function() {
       sandbox = sinon.sandbox.create();
-      sandbox.stub(db, "load");
-      app.set("data", []);
       app.start();
     });
 
     afterEach(function(done) {
       sandbox.restore();
-      app.set("data", []);
       try {
         app.close(done);
       } catch (err) {
@@ -42,140 +37,15 @@ describe("App", function() {
     });
 
     describe('GET /api/recent', function() {
-      it("should list recent packages", function(done) {
-        app.set('data', [
-          {slug: "a", createdAt: "2013-01-01T00:00:00Z"},
-          {slug: "b", createdAt: "2013-03-01T00:00:00Z"},
-          {slug: "c", createdAt: "2013-02-01T00:00:00Z"}
-        ]);
-        http.get('/api/recent', function(res) {
-          expect(res.headers['content-type']).to.equal('application/json');
-
-          expect(res.body).to.be.a('object');
-          expect(res.body).to.have.keys('total', 'packages');
-
-          expect(res.body.total).to.equal(3);
-          expect(res.body.packages).to.have.length.of(3);
-          expect(res.body.packages.map(function(x) {
-            return x.slug;
-          })).to.deep.equal(['b', 'c', 'a']);
-          done();
-        });
-      });
-
-      describe("Pagination", function() {
-        var perPageBackup;
-
-        beforeEach(function() {
-          perPageBackup = app.get('per page');
-          app.set('per page', 2);
-          app.set('data', range(9).map(function(x) {
-            x += 1;
-            return {slug: "p" + x, createdAt: "2013-01-0" + x + "T00:00:00Z"};
-          }));
-        });
-
-        afterEach(function() {
-          app.set('per page', perPageBackup);
-        });
-
-        it("should retrieve 1st page", function(done) {
-          http.get('/api/recent?offset=0', function(res) {
-            expect(res.body.total).to.equal(9);
-            expect(res.body.packages.map(function(x) {
-              return x.slug;
-            })).to.deep.equal(['p9', 'p8']);
-            done();
-          });
-        });
-
-        it("should retrieve 2nd page", function(done) {
-          http.get('/api/recent?offset=2', function(res) {
-            expect(res.body.total).to.equal(9);
-            expect(res.body.packages.map(function(x) {
-              return x.slug;
-            })).to.deep.equal(['p7', 'p6']);
-            done();
-          });
-        });
-
-        it("should retrieve 5th page", function(done) {
-          http.get('/api/recent?offset=8', function(res) {
-            expect(res.body.total).to.equal(9);
-            expect(res.body.packages.map(function(x) {
-              return x.slug;
-            })).to.deep.equal(['p1']);
-            done();
-          });
-        });
-      });
+      it("should list recent packages");
     });
 
     describe('GET /api/updated', function() {
-      it("should list updated packages", function(done) {
-        app.set('data', [
-          {slug: "a", updatedAt: "2013-01-01T00:00:00Z"},
-          {slug: "b", updatedAt: "2013-03-01T00:00:00Z"},
-          {slug: "c", updatedAt: "2013-02-01T00:00:00Z"}
-        ]);
-        http.get('/api/updated', function(res) {
-          expect(res.headers['content-type']).to.equal('application/json');
-
-          expect(res.body).to.be.a('object');
-          expect(res.body).to.have.keys('total', 'packages');
-
-          expect(res.body.total).to.equal(3);
-          expect(res.body.packages).to.have.length.of(3);
-          expect(res.body.packages.map(function(x) {
-            return x.slug;
-          })).to.deep.equal(['b', 'c', 'a']);
-          done();
-        });
-      });
+      it("should list updated packages");
     });
 
     describe('GET /api/popular', function() {
-      it("should list popular packages", function(done) {
-        app.set('data', [
-          {
-            slug: "a",
-            updatedAt: "2013-01-01T00:00:00Z",
-            createdAt: "2013-01-01T00:00:00Z",
-            nbStargazers: 1,
-            nbForks: 1,
-            nbIssues: 100
-          },
-          {
-            slug: "b",
-            updatedAt: "2013-01-01T00:00:00Z",
-            createdAt: "2013-01-01T00:00:00Z",
-            nbStargazers: 10000,
-            nbForks: 100,
-            nbIssues: 1
-          },
-          {
-            slug: "c",
-            updatedAt: "2013-01-01T00:00:00Z",
-            createdAt: "2013-01-01T00:00:00Z",
-            nbStargazers: 100,
-            nbForks: 1,
-            nbIssues: 1
-          }
-        ]);
-        http.get('/api/popular', function(res) {
-          expect(res.headers['content-type']).to.equal('application/json');
-
-          expect(res.body).to.be.a('object');
-          expect(res.body).to.have.keys('total', 'packages');
-
-          expect(res.body.total).to.equal(3);
-          expect(res.body.packages).to.have.length.of(3);
-          expect(res.body.packages.map(function(x) {
-            return x.slug;
-          })).to.deep.equal(['b', 'c', 'a']);
-          done();
-        });
-      });
+      it("should list popular packages");
     });
 
     describe('GET /api/search', function() {
